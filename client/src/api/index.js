f7f0e027f67d8e4c2317274c4cd91adddf5d711e
Nitish Ghosh <137692440+nitishgos@users.noreply.github.com>
2025-11-30 15:55:50 +0530

    # on branch main

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-    baseURL:"http://localhost:8080/api/"
+    baseURL:"https://ai-image-generator-9boj.onrender.com/api/"
 });
 export const GetPosts = async () => await API.get("/post/");
 export const CreatePost = async (data) => await API.post("/post/", data);
